4dda55069582f2e2713a5fcab8b6b654f422835e
Edward Knueppel <edward.knueppel@zywave.com>
2022-03-06 11:38:33 -0600

    Add project files.

---
+++ InformationSite/Pages/DotNet.cshtml.cs
@@ -0,0 +1,26 @@
+﻿namespace InformationSite.Pages
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class DotNetModel : PageModel
+    {
+        private readonly ILogger<DotNetModel> _logger;
+
+        public DotNetModel(ILogger<DotNetModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ InformationSite/Pages/Xamarin.cshtml.cs
@@ -0,0 +1,26 @@
+﻿namespace InformationSite.Pages
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using Microsoft.Extensions.Logging;
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class XamarinModel : PageModel
+    {
+        private readonly ILogger<XamarinModel> _logger;
+
+        public XamarinModel(ILogger<XamarinModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
